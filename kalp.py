7af771e02d7c757618f1f84d7bf2316e165da85a
Emine Topdemir <149799860+eminetopdemir@users.noreply.github.com>
2025-05-08 00:10:06 +0300

    Add files via upload

---
+++ kalp.py
@@ -0,0 +1,33 @@
+import math
+
+from turtle import *
+
+def heart_a(n):
+
+    return 15 * math.sin(n) ** 3
+
+
+def heart_b(n):
+
+    return 12 * math.cos(n)- 5 * \
+        math.cos(2*n)- 2 *\
+        math.cos(3*n)- \
+        math.cos(4*n)
+
+
+tracer (5)
+bgcolor("black")
+
+for i in range(1500):
+
+    goto (heart_a(i)*15, heart_b(i)*15)
+
+    for j in range(1):
+
+        color('red')
+
+        hideturtle()
+
+        goto(0,0)
+
+done()
